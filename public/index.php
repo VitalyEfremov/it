36739505dbcf0b4e6c019ee199dfb9782bc15ae6
Vitaly Efremov <fingolfin@yandex-team.ru>
2019-10-21 12:58:04 +0700

    gulp downloaded

--- public/index.php
+++ public/index.php
@@ -48,6 +48,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
+$a = 5;
+$a = $a + 2;
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
