4027b881ca37d751426f517a03959e62bf3ccef8
Vitaly Efremov <fingolfin@yandex-team.ru>
2019-10-27 15:25:14 +0700

    Добавил шаблоны и sass > css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,88 +1,16 @@
-<html>
-<head>
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $(".header").on("click","a", function (event) {
-                event.preventDefault();
-                var id  = $(this).attr('href'),
-                    top = $(id).offset().top;
-                $('body,html').animate({scrollTop: top}, 500);
-            });
-        });
-    </script>
-    <script type="text/javascript">
-        function dropMenu() {
-            display = document.getElementById('dropdown-menu').style.display;
-            var body = document.body, html = document.documentElement;
-            var height = Math.max( body.scrollHeight, body.offsetHeight,
-                html.clientHeight, html.scrollHeight, html.offsetHeight );
+@extends('layouts.main-page')
 
-            if (display == "none") {
-                document.getElementById('main-content').style.display = "none";
-                document.getElementById('dropdown-menu').style.height = height;
-                document.getElementById('dropdown-menu').style.display = "block";
-            } else {
-                document.getElementById('dropdown-menu').style.display = "none";
-                document.getElementById('main-content').style.display = "block";
-            }
-        }
-    </script>
-</head>
-<body>
-<div class="main-top-nav">
-    <nav>
-        <ul class="top-nav-bar">
-            <li class="logo-icon">
-                <a href="index.html"><img src="assets/imgs/logo_top.png"></a>
-            </li>
-            <li class="site-pages">
-                <div><a href="#">О НАС</a></div>
-                <div><a href="#">НАШИ ПРОДУКТЫ</a></div>
-                <div><a href="#">НАШИ ДИСТРИБЬЮТЕРЫ</a></div>
-                <div><a href="#">КАРЬЕРА В ЭЛАНКО</a></div>
-                <div><a href="#">НАШИ КОНТАКТЫ</a></div>
-            </li>
-            <li id="top-right-icons">
-                <div class="drop-menu-icon">
-                    <a href="#" onclick="dropMenu()"><img src="assets/icons/dropdown-menu-icon.png"></a>
-                </div>
-                <div class="search-icon">
-                    <a href="#"><img src="assets/icons/serach-icon.png"></a>
-                </div>
-            </li>
-        </ul>
-    </nav>
-</div>
-<div id="dropdown-menu" style="display: none;">
-    <ul class="dropdown-menu-columns">
-        <li><a class="title">О НАС</a></li>
-        <li><a class="title">НАШИ ПРОДУКТЫ</a>
-            <ul>
-                <li><a href="#">ПТИЦЕВОДСТВА</a></li>
-                <li><a href="#">СВИНОВОДСТВА</a></li>
-                <li><a href="#">ЖИВОТНОВОДСТВА</a></li>
-                <li><a href="#">СОБАК И КОШЕК</a></li>
-            </ul>
-        </li>
-        <li><a class="title">НАШИ ДИСТРИБЬЮТЕРЫ</a></li>
-        <li><a class="title">КАРЬЕРА В ЭЛАНКО</a></li>
-        <li><a class="title">НАШИ КОНТАКТЫ</a></li>
-        <div class="clearfix"></div>
-    </ul>
-</div>
-<div id="main-content">
+@section('main-content')
     <div class="header">
         <div class="bg-image"></div>
         <div class="text-center">
             <div class="logo">
-                <img src="assets/imgs/elanco__1_.svg">
+                <img src="../../public/assets/imgs/elanco__1_.svg">
             </div>
             <h1 class="h1-top">Обогащая жизнь</h1>
         </div>
         <div class="chevron-down-icon">
-            <a href="#main"><img src="assets/icons/chevron-down-icon.png"></a>
+            <a href="#main"><img src="../../public/assets/icons/chevron-down-icon.png"></a>
         </div>
     </div>
     <div class="main-1-block" id="main">
@@ -133,7 +61,7 @@
             <br>
             <br>
             <div class="tablet-image">
-                <img src="assets/imgs/mission.png">
+                <img src="../../public/assets/imgs/mission.png">
             </div>
             <div class="mission-and-vision">
                 <span>Наша миссия:</span>
@@ -166,7 +94,7 @@
     </div>
     <div class="info-columns">
         <div class="column">
-            <a href="#"><img src="assets/imgs/info-dog-image.jpg"></a>
+            <a href="#"><img src="../../public/assets/imgs/info-dog-image.jpg"></a>
             <h3 class="info-header">
                 Пусть ваш питомец будет здоровым с Мильбемакс®
             </h3>
@@ -183,7 +111,7 @@
             </p>
         </div>
         <div class="column">
-            <a href="#"><img src="assets/imgs/salmonela.png"></a>
+            <a href="#"><img src="../../public/assets/imgs/salmonela.png"></a>
             <h3 class="info-header">
                 Сальмонелла360°
             </h3>
@@ -200,7 +128,7 @@
             </p>
         </div>
         <div class="column">
-            <a href="#"><img src="assets/imgs/info-logo.png"></a>
+            <a href="#"><img src="../../public/assets/imgs/info-logo.png"></a>
             <h3 class="info-header">
                 Новости Эланко
             </h3>
@@ -215,27 +143,4 @@
         </div>
         <div class="clearfix"></div>
     </div>
-    <footer>
-        <div class="line-1">
-            <div>
-                <ul>
-                    <li><a href="#">Политика конфиденциальности</a></li>
-                    <li><a href="#">Пользовательское соглашение</a></li>
-                    <li><a href="#">Товарные знаки</a></li>
-                </ul>
-            </div>
-            <div>
-                <ul>
-                    <li><a href="#">Свяжитесь с нами</a></li>
-                    <li><a href="#">Эланко в мире</a></li>
-                    <li><a href="index.html"><img></a></li>
-                </ul>
-            </div>
-            <div class="clearfix"></div>
-        </div>
-        <div class="line-2">Copyright</div>
-        <div class="clearfix"></div>
-    </footer>
-</div>
-</body>
-</html>
+@endsection
---
+++ resources/views/layouts/main-page.blade.php
@@ -0,0 +1,101 @@
+<html lang="en">
+<head>
+    <title>Main page</title>
+    <link rel="stylesheet" href="../../public/css/app.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(".header").on("click","a", function (event) {
+                event.preventDefault();
+                var id  = $(this).attr('href'),
+                    top = $(id).offset().top;
+                $('body,html').animate({scrollTop: top}, 500);
+            });
+        });
+    </script>
+    <script type="text/javascript">
+        function dropMenu() {
+            display = document.getElementById('dropdown-menu').style.display;
+            var body = document.body, html = document.documentElement;
+            var height = Math.max( body.scrollHeight, body.offsetHeight,
+                html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+            if (display == "none") {
+                document.getElementById('main-content').style.display = "none";
+                document.getElementById('dropdown-menu').style.height = height;
+                document.getElementById('dropdown-menu').style.display = "block";
+            } else {
+                document.getElementById('dropdown-menu').style.display = "none";
+                document.getElementById('main-content').style.display = "block";
+            }
+        }
+    </script>
+</head>
+<body>
+<div class="main-top-nav">
+    <nav>
+        <ul class="top-nav-bar">
+            <li class="logo-icon">
+                <a href="index.html"><img src="../../../public/assets/imgs/logo_top.png"></a>
+            </li>
+            <li class="site-pages">
+                <div><a href="#">О НАС</a></div>
+                <div><a href="#">НАШИ ПРОДУКТЫ</a></div>
+                <div><a href="#">НАШИ ДИСТРИБЬЮТЕРЫ</a></div>
+                <div><a href="#">КАРЬЕРА В ЭЛАНКО</a></div>
+                <div><a href="#">НАШИ КОНТАКТЫ</a></div>
+            </li>
+            <li id="top-right-icons">
+                <div class="drop-menu-icon">
+                    <a href="#" onclick="dropMenu()"><img src="../../../public/assets/icons/dropdown-menu-icon.png"></a>
+                </div>
+                <div class="search-icon">
+                    <a href="#"><img src="../../../public/assets/icons/serach-icon.png"></a>
+                </div>
+            </li>
+        </ul>
+    </nav>
+</div>
+<div id="dropdown-menu" style="display: none;">
+    <ul class="dropdown-menu-columns">
+        <li><a class="title">О НАС</a></li>
+        <li><a class="title">НАШИ ПРОДУКТЫ</a>
+            <ul>
+                <li><a href="#">ПТИЦЕВОДСТВА</a></li>
+                <li><a href="#">СВИНОВОДСТВА</a></li>
+                <li><a href="#">ЖИВОТНОВОДСТВА</a></li>
+                <li><a href="#">СОБАК И КОШЕК</a></li>
+            </ul>
+        </li>
+        <li><a class="title">НАШИ ДИСТРИБЬЮТЕРЫ</a></li>
+        <li><a class="title">КАРЬЕРА В ЭЛАНКО</a></li>
+        <li><a class="title">НАШИ КОНТАКТЫ</a></li>
+        <div class="clearfix"></div>
+    </ul>
+</div>
+<div id="main-content">
+    @yield('main-content')
+    <footer>
+        <div class="line-1">
+            <div>
+                <ul>
+                    <li><a href="#">Политика конфиденциальности</a></li>
+                    <li><a href="#">Пользовательское соглашение</a></li>
+                    <li><a href="#">Товарные знаки</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">Свяжитесь с нами</a></li>
+                    <li><a href="#">Эланко в мире</a></li>
+                    <li><a href="index.html"><img></a></li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="line-2">Copyright</div>
+        <div class="clearfix"></div>
+    </footer>
+</div>
+</body>
+</html>
